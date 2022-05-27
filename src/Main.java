b5fd9abe5bc835796744c4d83f0ccc3c0ca9d2b7
J-ordiEs <jorge.tm.ink@outlook.com>
2022-05-27 16:59:57 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int result  = sum(505, 60, 5);
+        System.out.println(result);
+
+    }
+    public static int sum(int a, int b, int c){
+        return a + b + c;
+    }
+}
