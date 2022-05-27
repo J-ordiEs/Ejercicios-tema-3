a492788b5a436fccee510c4d8fc73092d1390c79
J-ordiEs <jorge.tm.ink@outlook.com>
2022-05-27 17:25:38 +0200

    solucion ejercicios tema 3

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,29 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Primera parte
         int result  = sum(505, 60, 5);
         System.out.println(result);
 
+        //Segunda parte
+        Car camaro = new Car();
+        camaro.addDoors();
+        camaro.addDoors();
+        System.out.println(camaro.doors);
+
     }
+
     public static int sum(int a, int b, int c){
         return a + b + c;
+    }
+
+}
+
+class Car{
+
+    public int doors = 2 ;
+
+    public void  addDoors(){
+        this.doors++;
     }
 }
